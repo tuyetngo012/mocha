b88d7b673924bb83828d3cc24e419ad662a52093
Christopher Hiller <boneskull@boneskull.com>
2018-01-18 21:52:21 -0800

    fix color test

--- test/node-unit/color.spec.js
+++ test/node-unit/color.spec.js
@@ -5,22 +5,35 @@ var childProcess = require('child_process');
 var path = require('path');
 
 describe('Mocha', function () {
+  var FORCE_COLOR;
+
   this.timeout(4000);
+
+  before(function () {
+    // avoid forcing the hand of `supports-color`
+    FORCE_COLOR = process.env.FORCE_COLOR;
+    delete process.env.FORCE_COLOR;
+  });
+
+  after(function () {
+    process.env.FORCE_COLOR = FORCE_COLOR;
+  });
 
   it('should not output colors to pipe', function (done) {
     var command = [path.join('bin', 'mocha'), '--grep', 'missing-test'];
 
-    // avoid forcing the hand of `supports-color`
-    delete process.env.FORCE_COLOR;
+    childProcess.execFile(
+      process.execPath,
+      command,
+      function (err, stdout, stderr) {
+        if (err) {
+          return done(err);
+        }
 
-    childProcess.execFile(process.execPath, command, function (err, stdout, stderr) {
-      if (err) {
-        return done(err);
+        assert(stdout.indexOf('[90m') === -1);
+
+        done();
       }
-
-      assert(stdout.indexOf('[90m') === -1);
-
-      done();
-    });
+    );
   });
 });
